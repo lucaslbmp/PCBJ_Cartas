d2d8a4e1f46bb4e8a8ccb387f104bc0164c53e0f
lucas <lucasset10@msn.com>
2021-02-23 15:28:34 -0300

    Commit inicial

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject carta; // carta a ser descartada
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MostraCartas();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MostraCartas()
+    {
+        int[] arrayEmbaralhado = criaArrayEmbaralhado();
+        int[] arrayEmbaralhado2 = criaArrayEmbaralhado();
+        //Instantiate(carta, new Vector3(0, 0, 0), Quaternion.identity);
+        for (int i = 0; i < 13; i++)
+        {
+            //AddUmaCarta(i);
+            AddUmaCarta(0,i,arrayEmbaralhado[i]);
+            AddUmaCarta(1,i, arrayEmbaralhado2[i]);
+        }
+        
+    }
+
+    public void AddUmaCarta(int linha,int rank,int valor)
+    {
+        GameObject centro = GameObject.Find("centroDaTela");
+        float escalaCartaOriginal = carta.transform.localScale.x;
+        float fatorEscalaX = (658 * escalaCartaOriginal) / 110f;
+        float fatorEscalaY = (945 * escalaCartaOriginal) / 110f;
+        //Vector3 novaPosicao = new Vector3(centro.transform.position.x + (rank - 13 / 2) * 1.8f, centro.transform.position.y, centro.transform.position.z);
+        Vector3 novaPosicao = new Vector3(centro.transform.position.x + (rank - 13 / 2) * fatorEscalaX, centro.transform.position.y + (linha-2/2)*fatorEscalaY, centro.transform.position.z);
+        //GameObject c = (GameObject)Instantiate(carta, new Vector3(0, 0, 0), Quaternion.identity);
+        GameObject c = (GameObject)Instantiate(carta, novaPosicao, Quaternion.identity);
+        c.tag = "" + (valor+1);
+        c.name = "" + linha + "_" + valor;
+        string numeroDaCarta = "";
+        string nomeDaCarta = "";
+        /*if (rank == 0)
+            numeroDaCarta = "ace";
+        else if (rank == 10)
+            numeroDaCarta = "jack";
+        else if (rank == 11)
+            numeroDaCarta = "queen";
+        else if (rank == 12)
+            numeroDaCarta = "king";
+        else
+            numeroDaCarta = "" + (rank+1);
+        nomeDaCarta = numeroDaCarta + "_of_clubs";
+        Sprite s1 = (Sprite)Resources.Load<Sprite>(nomeDaCarta);
+        print("S: " + s1);
+        GameObject.Find(""+rank).GetComponent<Tile>().setOriginalCarta(s1);*/
+        if (valor == 0)
+            numeroDaCarta = "ace";
+        else if (valor == 10)
+            numeroDaCarta = "jack";
+        else if (valor == 11)
+            numeroDaCarta = "queen";
+        else if (valor == 12)
+            numeroDaCarta = "king";
+        else
+            numeroDaCarta = "" + (valor + 1);
+        nomeDaCarta = numeroDaCarta + "_of_clubs";
+        Sprite s1 = (Sprite)Resources.Load<Sprite>(nomeDaCarta);
+        print("S: " + s1);
+        GameObject.Find("" + linha + "_" + valor).GetComponent<Tile>().setOriginalCarta(s1);
+    }
+
+    public int[] criaArrayEmbaralhado()
+    {
+        int[] novoArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        int temp;
+        for(int t = 0; t < 13; t++) // Para cada posiçao de carta t:
+        {
+            temp = novoArray[t]; // Armazenar o numnero da carta na posição t numa variavel temporaria
+            int r = Random.Range(t,13); // Selecionar uma posiçao aleatoria de carta no descarte
+            novoArray[t] = novoArray[r]; // O número da posição t recebe o número da carta na posição aleatoriamente selecionada
+            novoArray[r] = temp; // O número da carta na posiçao aleatoriamente selecionada recebe o número da carta na posição t
+        }
+        return novoArray;
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private bool tileRevelada = false;
+    public Sprite originalCarta; // sprite da carta desejada
+    public Sprite backCarta; // sprite do verso da carta
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EscondeCarta();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        print("Voce pressionou a telha");
+        if (tileRevelada)
+        {
+            EscondeCarta();
+        }
+        else
+        {
+            RevelaCarta();
+        }
+    }
+
+    public void EscondeCarta()
+    {
+        GetComponent<SpriteRenderer>().sprite = backCarta;
+        tileRevelada = false;
+    }
+
+    public void RevelaCarta()
+    {
+        GetComponent<SpriteRenderer>().sprite = originalCarta;
+        tileRevelada = true;
+    }
+
+    public void setOriginalCarta(Sprite novaCarta)
+    {
+        originalCarta = novaCarta;
+    }
+}
